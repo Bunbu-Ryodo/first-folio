339b77abbd6bb138b073c2d532a19d9b1e9ca528
Jay Lacey <jaylacey22@pm.me>
2023-12-26 12:34:59 +0000

    Finished login and register forms

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -70,7 +70,6 @@ export async function createUser(prevState: State, formData: FormData){
           }
         }
         revalidatePath('/register');
-        //If something suddenly breaks look here
         redirect('/introduce');
       }
 
@@ -88,8 +87,6 @@ export async function createUser(prevState: State, formData: FormData){
           password: formData.get('password')
         })
         
-        console.log(validatedFields, "What are we validating???");
-
         if(!validatedFields.success){
           return {
             errors: validatedFields.error.flatten().fieldErrors,
@@ -99,11 +96,7 @@ export async function createUser(prevState: State, formData: FormData){
 
         const { email, password } = validatedFields.data;
 
-        console.log(email, "Email is this");
-        console.log(password, "Password is this");
-
         try {
-          console.log("Do we hit the try block? Yes it seems");
           const user = await prisma.user.findUnique({
             where: {
               email: email
@@ -111,28 +104,19 @@ export async function createUser(prevState: State, formData: FormData){
           })
           //If weird errors, try moving this outside try
           if(user){
-
-            console.log(user, "Have we found a user? It seems so");
-
             const hashedPassword = user.password
-
-            console.log(hashedPassword, "This is the hashed password");
 
             bcrypt.compare(password, hashedPassword).then(function(hash: boolean){
               if(hash == false){
-                console.log("This should definitely fail, if both fields are blank");
                 return { errors: { password: ['Password is incorrect']}, message: 'Invalid password, please try again'}
               } else {
-                console.log("I shouldn't see this, if both fields are blank");
                 return { errors: {}, message: 'Log in successful'}
               }
             })
           } else {
-            console.log("I should see this if an email is not in the database");
             return { errors: { email: ['No user with that email exists']}, message: 'Invalid login, please try again'}
           }
         } catch(e: any){
-            console.log("I presume I'll see this if the form fields are empty");
             return { errors: {}, message: 'Unable to login please try again' }
         }
 
