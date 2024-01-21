1852275aec72b1484677898f6ba029fb2f8b46dc
Jay Lacey <jaylacey22@pm.me>
2024-01-21 22:11:32 +0000

    Done for now

--- app/ui/general-ui/next-button.tsx
+++ app/ui/general-ui/next-button.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
 
-export default function NextButton({ link }: { link: string }) {
+export default function NextButton({
+  link,
+  newTab,
+}: {
+  link: string;
+  newTab?: boolean;
+}) {
   return (
     <div className="flex flex-row-reverse w-full">
-      <Link href={`/${link}`} className="w-1/3">
+      <Link href={`/${link}`} className="w-1/3" target={newTab ? "_blank" : ""}>
         <button className="bg-monokaiGreen text-button text-monokaiBlack rounded h-button hover:bg-buttonGreenHover active:bg-buttonGreenActive my-nextButton-y mr-nextButton w-full">
           Next()
         </button>
