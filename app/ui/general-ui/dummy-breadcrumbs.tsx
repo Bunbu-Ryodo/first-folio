89d0edc26f628fd2f6cc4ed1f08aa5cac44d00ea
Jay Lacey <jaylacey22@pm.me>
2024-01-23 15:25:39 +0000

    Fixed the blink animation

---
+++ app/ui/general-ui/dummy-breadcrumbs.tsx
@@ -0,0 +1,9 @@
+export default function DummyBreadCrumbs() {
+  return (
+    <div className="w-full flex ml-breadcrumbs mt-breadcrumbs animate-pulse">
+      <span className="opacity-0 text-breadCrumb text-monokaiBlue underline">
+        Loading...
+      </span>
+    </div>
+  );
+}
---
+++ app/ui/general-ui/dummy-submit-button.tsx
@@ -0,0 +1,8 @@
+export default function DummySubmitButton() {
+  return (
+    <button
+      type="submit"
+      className="flex-1 bg-monokaiGreen text-button text-monokaiBlack rounded h-button hover:bg-buttonGreenHover active:bg-buttonGreenActive mr-[16px] disabled:bg-slate-500 w-full"
+    ></button>
+  );
+}
