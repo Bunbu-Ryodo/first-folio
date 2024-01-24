8bdfad19033d8fd353ca4541c7a80126d7686bdb
Jay Lacey <jaylacey22@pm.me>
2024-01-24 13:08:05 +0000

    Completed static layout but for project modals

--- app/ui/portfolio/contact-form.tsx
+++
@@ -1,9 +0,0 @@
-export default function ContactForm() {
-  return (
-    <div className="flex justify-center items-center border-t-[1px] border-portfolioBlack">
-      <div className="flex flex-col w-full md:w-2/3">
-        <label>Contact E-mail:</label>
-      </div>
-    </div>
-  );
-}
